1cc747115e9479a935453f0cc271e878c283d1a1
Derek Bruckner <dbrucknr@umich.edu>
2021-06-13 16:32:40 -0400

    begin adding custom css styles and layout updates.

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,16 @@ import Header from "./Header";
 import Footer from "./Footer";
 import { useRoutes } from "react-router-dom";
 import AppRoutes from "../router/routes"
+import "../css/app.css"
 
 const App = () => {
   let renderedComponent = useRoutes(AppRoutes);
   return (
     <div className="App">
       <Header />
+      <div className="container">
         { renderedComponent }
+      </div>
       <Footer />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
       <div className="topnav">
-        <Link className="active" to="/" replace={true}>Home</Link>
-        <a href="#news">Resume</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
-        <Link to="/about" replace={true}>About Me</Link>
+          <a href="#news">Resume</a>
+          <a href="#projects">Projects</a>
+          <Link className="active" to="/" replace={true}>Home</Link>
+          <a href="#contact">Contact</a>
+          <Link to="/about" replace={true}>About Me</Link>
       </div>
     );
   }
